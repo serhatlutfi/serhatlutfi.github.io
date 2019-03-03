b05a6630c2a9e7a88ea8dae2730c11178cbbbb35
serhatlutfi <47432421+serhatlutfi@users.noreply.github.com>
2019-03-03 14:23:40 +0200

    eklemeler

---
+++ js/scr.js
@@ -0,0 +1,103 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+var slideInd = 1;
+showSlide(slideInd);
+
+function plSlides(m) {
+  showSlide(slideInd += m);
+}
+
+function curSlide(m) {
+  showSlide(slideInd = m);
+}
+
+function showSlide(m) {
+  var j;
+  var slide = document.getElementsByClassName("slaytlar");
+  var dot = document.getElementsByClassName("dem");
+  var captionTxt = document.getElementById("cap");
+  if (m > slide.length) {slideInd = 1}
+  if (m < 1) {slideInd = slide.length}
+  for (j = 0; j < slide.length; j++) {
+      slide[j].style.display = "none";
+  }
+  for (j = 0; j < dot.length; j++) {
+      dot[j].className = dot[j].className.replace(" aktif", "");
+  }
+  slide[slideInd-1].style.display = "block";
+  dot[slideInd-1].className += " aktif";
+  captionTxt.innerHTML = dot[slideInd-1].alt;
+}
+
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+jssor_1_slider_init();
+
+
+        jssor_1_slider_init = function() {
+
+            var jssor_1_SlideshowTransitions = [
+              {$Duration:800,$Opacity:2}
+            ];
+
+            var jssor_1_options = {
+              $AutoPlay: 1,
+              $SlideshowOptions: {
+                $Class: $JssorSlideshowRunner$,
+                $Transitions: jssor_1_SlideshowTransitions,
+                $TransitionsOrder: 1
+              },
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+              }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            /*#region responsive code begin*/
+
+            var MAX_WIDTH = 1070;
+
+            function ScaleSlider() {
+                var containerElement = jssor_1_slider.$Elmt.parentNode;
+                var containerWidth = containerElement.clientWidth;
+
+                if (containerWidth) {
+
+                    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+                    jssor_1_slider.$ScaleWidth(expectedWidth);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+
+            ScaleSlider();
+
+            $Jssor$.$AddEvent(window, "load", ScaleSlider);
+            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+            /*#endregion responsive code end*/
+        };
